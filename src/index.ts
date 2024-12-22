6df699544b216ce21e70f21d80f6969397964a61
zanna927 <s.zanetti@relewant.com>
2024-12-22 21:24:23 +0100

    feat: edited typescript files

--- src/index.ts
+++ src/index.ts
@@ -12,32 +12,30 @@ app.use(express.json());
 console.log("Current Firebase Project:", admin.app().options.projectId);
 
 // Create Box
-app.post('/box', async (req, res) => {
-    try {
-        const { title, type, date, time, category } = req.body;
-    
-        /*if (!title || !message) {
+app.post("/box", async (req, res) => {
+  try {
+    const { title, type, date, time, category } = req.body;
+
+    /*if (!title || !message) {
           return res.status(400).json({ error: "Name and expiration are required" });
         }*/
-    
-        const box: Box = {
-            title: title,
-            type: type,
-            date: date,
-            time: time,
-            category: category
-        };
-    
-        const docRef = await db.collection("boxes").add(box);
-        return res.status(201).json({ id: docRef.id, ...box });
-      } catch (error) {
-        console.error("Error creating box:", error);
-        return res.status(500).json({ error: "Internal Server Error" });
-      }
+
+    const box: Box = {
+      title: title,
+      type: type,
+      date: date,
+      time: time,
+      category: category,
+    };
+
+    const docRef = await db.collection("boxes").add(box);
+    return res.status(201).json({ id: docRef.id, ...box });
+  } catch (error) {
+    console.error("Error creating box:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-app.get('/box', async (req, res) => {
-
-});
+app.get("/box", async (req, res) => {});
 
 exports.api = onRequest(app);
--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,13 @@
 export interface Box {
-    title: string;
-    user?: string;
-    message?: string;
-    type: string;
-    category: string;
-    date: string;
-    time: string;
-    filePath?: string;
-    isAnonymous?: boolean;
-    createdAt?: FirebaseFirestore.FieldValue;
-    updatedAt?: FirebaseFirestore.FieldValue;
+  title: string;
+  user?: string;
+  message?: string;
+  type: string;
+  category: string;
+  date: string;
+  time: string;
+  filePath?: string;
+  isAnonymous?: boolean;
+  createdAt?: FirebaseFirestore.FieldValue;
+  updatedAt?: FirebaseFirestore.FieldValue;
 }
