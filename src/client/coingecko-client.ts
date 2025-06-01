62b17aed4af7b3516da4c1d0e5ba0c915e1f1c86
zasi <simone.zanetti.relewant@corner.ch>
2025-06-01 10:23:28 +0200

    feat: added cache for price

--- src/client/coingecko-client.ts
+++ src/client/coingecko-client.ts
@@ -1,6 +1,15 @@
 import axios from "axios";
+import { LRUCache } from "lru-cache";
+
+const cache = new LRUCache<string, number>({
+  max: 1,
+  ttl: 60_000,
+});
 
 const getSolanaPrice = async (): Promise<number> => {
+  const cached = cache.get("solPrice");
+  if (cached !== undefined) return cached;
+
   try {
     const response = await axios.get(
       "https://api.coingecko.com/api/v3/simple/price",
@@ -13,7 +22,9 @@ const getSolanaPrice = async (): Promise<number> => {
     );
 
     const price: number = response.data.solana.eur;
-    console.log("SOL Price (EUR):", price);
+    console.log("SOL Price (EUR): ", price);
+    cache.set("solPrice", price);
+
     return price;
   } catch (error) {
     console.error("Error while retrieving SOL Price: ", error);
